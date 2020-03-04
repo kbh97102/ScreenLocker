587cb74a6df2b64a14a7c1c6a4a5f5cd66dfb95d
Arakene <kbh97102@naver.com>
2020-03-04 22:10:09 +0900

    Update MainActivity

--- Calculator/app/src/main/java/com/example/calcultor/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calcultor/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.calcultor
 
 import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.collections.HashMap
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +16,11 @@ class MainActivity : AppCompatActivity() {
     private var firstNumber = 0
     private var secondNumber = 0
     private var currentFeature: Button? = null
+    private var isFirstFilled = false
+    private var digit = 0
+
+    private var firstNumber2: String = ""
+    private var secondNumber2: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,26 +55,54 @@ class MainActivity : AppCompatActivity() {
                 setNumberButtonEvent(btn)
             }
         }
-        plus.setOnClickListener { currentFeature = plus }
-        minus.setOnClickListener { currentFeature = minus }
-        division.setOnClickListener { currentFeature = division }
-        multiply.setOnClickListener { currentFeature = multiply }
-        equal.setOnClickListener { calculate() }
+        plus.setOnClickListener { setFeatureEvent(plus) }
+        minus.setOnClickListener { setFeatureEvent(minus) }
+        division.setOnClickListener { setFeatureEvent(division) }
+        multiply.setOnClickListener { setFeatureEvent(multiply) }
+        equal.setOnClickListener { calculate2() }
     }
 
     private fun setNumberButtonEvent(btn: Button) {
-        if(firstNumber == 0){
-            firstNumber = numberValue[btn]!!
-            numberView.setText(firstNumber.toString())
-        }
-        else{
-            secondNumber = numberValue[btn]!!
-            numberView.setText(secondNumber.toString())
+        if (isFirstFilled) {
+//            secondNumber += getNumber(numberValue[btn]!!, secondNumber)
+//            numberView.setText(secondNumber.toString())
+//            processView.append(numberValue[btn].toString())
+            secondNumber2 += btn.text
+            numberView.setText(secondNumber2)
+            processView.append(btn.text)
+        } else {
+//            firstNumber += getNumber(numberValue[btn]!!, firstNumber)
+//            numberView.setText(firstNumber.toString())
+//            processView.append(numberValue[btn].toString())
+            firstNumber2 += btn.text
+            numberView.setText(firstNumber2)
+            processView.append(btn.text)
         }
     }
 
+    private fun getNumber(number: Int, target: Int): Int {
+        if (number == 0) {
+            return (target * 10.toDouble().pow(digit.toDouble()).toInt()) - target
+        }
+        var result = 0
+        result = if (digit > 0) {
+            (number * 10.toDouble().pow(digit.toDouble())).toInt()
+        } else {
+            number
+        }
+        digit++
+        return result
+    }
+
+    private fun setFeatureEvent(btn: Button) {
+        isFirstFilled = true
+        digit = 0
+        currentFeature = btn
+        processView.append(btn.text)
+    }
+
     private fun calculate() {
-        if (firstNumber == 0 || secondNumber == 0 || Objects.isNull(currentFeature)){
+        if (firstNumber == 0 || secondNumber == 0 || Objects.isNull(currentFeature)) {
             return
         }
         var calculateResult = 0
@@ -83,6 +115,27 @@ class MainActivity : AppCompatActivity() {
         numberView.setText(calculateResult.toString())
         firstNumber = calculateResult
         secondNumber = 0
+        digit = 0
+        processView.append("=")
+        processView.append(calculateResult.toString())
     }
 
+    private fun calculate2() {
+        if (firstNumber2 == "" || secondNumber2 == "" || Objects.isNull(currentFeature)) {
+            return
+        }
+        var calculateResult = 0
+        when (currentFeature) {
+            plus -> calculateResult = firstNumber2.toInt() + secondNumber2.toInt()
+            minus -> calculateResult = firstNumber2.toInt() - secondNumber2.toInt()
+            division -> calculateResult = firstNumber2.toInt() / secondNumber2.toInt()
+            multiply -> calculateResult = firstNumber2.toInt() * secondNumber2.toInt()
+        }
+        numberView.setText(calculateResult.toString())
+        processView.append("=")
+        processView.append(calculateResult.toString())
+        firstNumber2 = calculateResult.toString()
+        secondNumber2 = ""
+        digit = 0
+    }
 }
